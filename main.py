8d7136ee0db45c05e9c31a9cdca467a682aa5340
Shreya Pekhale <95264365+spekhale@users.noreply.github.com>
2023-09-19 20:01:30 -0600

    final commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 print(requests.__version__)
 
-res = requests.get("https://raw.githubusercontent.com/spekhale/CMPUT404_lab1/main/test2.py?token=GHSAT0AAAAAACH2BZFPKWMGFN3GQMFP5JWWZIKLZ3Q")
+res = requests.get("https://raw.githubusercontent.com/spekhale/CMPUT404_lab1/main/main.py?token=GHSAT0AAAAAACH2BZFOSJI3IHEJB3JE75DQZIKMB2A")
 
 print(res.text)
 
